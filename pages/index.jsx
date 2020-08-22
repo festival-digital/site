b8ecd70b64c7e102503e60c98d22fdcfbcd65b85
Gabriel <gabrielfurlan05@gmail.com>
2020-08-21 21:06:52 -0300

    adding design system

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { Button, defaultTheme } from '@resystem/design-system';
 import Headers from 'components/Header';
 
+console.log(defaultTheme);
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -10,10 +13,11 @@ const Container = styled.div`
 `;
 
 const Home = () => (
-  <Container>
+  <ThemeProvider theme={defaultTheme}>
     <Headers />
     asdsad
-  </Container>
+    <Button>Testando isso aqui!</Button>
+  </ThemeProvider>
 );
 
 export default Home;
