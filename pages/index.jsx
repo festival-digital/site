74949553c76585f0b9b0bbc708e867a8698694e3
Gabriel <gabrielfurlan05@gmail.com>
2020-09-09 15:59:44 -0300

    remove resystem module

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
-import { Button, defaultTheme } from '@resystem/design-system';
 import Headers from 'components/Header';
 
-console.log(defaultTheme);
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -13,10 +11,9 @@ const Container = styled.div`
 `;
 
 const Home = () => (
-  <ThemeProvider theme={defaultTheme}>
+  <ThemeProvider theme={{}}>
     <Headers />
     asdsad
-    <Button>Testando isso aqui!</Button>
   </ThemeProvider>
 );
 
