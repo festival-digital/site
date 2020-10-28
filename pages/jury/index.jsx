0ae8f9b753c94309a0542a0b4d5c907e7bac2b5e
Mauricio Maletta <mauricio_maletta@hotmail.com>
2020-10-28 16:44:25 -0300

    some changes

--- pages/jury/index.jsx
+++ pages/jury/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import JuryTemplate from 'components/templates/jury/jury';
+import JuryTemplate from 'components/templates/jury/Jury';
 
 const Jury = () => <JuryTemplate />;
 
