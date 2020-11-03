476f7a8201dd003cb15bb91e618091a63d3e6ba0
Gabriel <gabrielfurlan05@gmail.com>
2020-11-03 15:08:41 -0300

    fixing jury

--- components/templates/jury/jury-template.jsx
+++ components/templates/jury/jury-template.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Button from 'components/atoms/button/button';
 import JuryCard from 'components/molecules/jury-card/jury-card';
 import Store from 'components/store/Store';
